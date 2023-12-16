9c9b77d73820ccc1ffe1d2c83179045214060301
koshi <chayakoshala.pvt@gmail.com>
2023-12-16 13:19:56 +0530

    Android ecome 1

--- app/src/main/java/com/example/ecomee/activities/MainActivity.java
+++ app/src/main/java/com/example/ecomee/activities/MainActivity.java
@@ -1,15 +1,15 @@
 package com.example.ecomee.activities;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 
-import android.os.Bundle;
-import android.widget.GridLayout;
-
-import com.example.ecomee.R;
 import com.example.ecomee.adapters.CategoryAdapter;
+import com.example.ecomee.adapters.ProductAdapter;
 import com.example.ecomee.databinding.ActivityMainBinding;
 import com.example.ecomee.model.Category;
+import com.example.ecomee.model.Product;
 
 import java.util.ArrayList;
 
@@ -19,6 +19,10 @@ public class MainActivity extends AppCompatActivity {
     CategoryAdapter categoryAdapter;
     ArrayList<Category> categories;
 
+    ProductAdapter productAdapter;
+
+    ArrayList<Product> products;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +30,11 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        initCategories();
+        initProducts();
+    }
+
+    void initCategories(){
         categories = new ArrayList<>();
         categories.add(new Category("baby","https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/How_to_use_icon.svg/2214px-How_to_use_icon.svg.png","#fe438e","Groomer Baby Clothing | Babies 0-24 Months | Preemie Baby Clothing",1));
         categories.add(new Category("baby","https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/How_to_use_icon.svg/2214px-How_to_use_icon.svg.png","#18ab4e","Groomer Baby Clothing | Babies 0-24 Months | Preemie Baby Clothing",1));
@@ -39,4 +48,19 @@ public class MainActivity extends AppCompatActivity {
         binding.categoriesList.setLayoutManager(layoutManager);
         binding.categoriesList.setAdapter(categoryAdapter);
     }
+    void  initProducts(){
+        products = new ArrayList<>();
+        products.add(new Product("baby","https://thumbs.dreamstime.com/b/top-view-fashion-trendy-look-kids-clothes-103930087.jpg","READY STOCK",52,54,20,1));
+        products.add(new Product("baby","https://thumbs.dreamstime.com/b/top-view-fashion-trendy-look-kids-clothes-103930087.jpg","READY STOCK",52,20,20,2));
+        products.add(new Product("baby","https://thumbs.dreamstime.com/b/top-view-fashion-trendy-look-kids-clothes-103930087.jpg","READY STOCK",52,30,20,3));
+        products.add(new Product("baby","https://thumbs.dreamstime.com/b/top-view-fashion-trendy-look-kids-clothes-103930087.jpg","READY STOCK",52,10,20,4));
+        products.add(new Product("baby","https://thumbs.dreamstime.com/b/top-view-fashion-trendy-look-kids-clothes-103930087.jpg","READY STOCK",52,15,20,5));
+        productAdapter = new ProductAdapter(this,products);
+
+
+        GridLayoutManager layoutManager = new GridLayoutManager(this,2);
+        binding.productList.setLayoutManager(layoutManager);
+        binding.productList.setAdapter(productAdapter);
+    }
+
 }
